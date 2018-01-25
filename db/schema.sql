b3559063bb404369ec9fa961d8fe2aa1d636b1b5
pablackhawk <l.tirtarahardja@gmail.com>
2018-01-24 23:13:12 -0800

    Removed test html file

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+  DROP DATABASE IF EXISTS burgers_db;
+  CREATE DATABASE burgers_db;
+  USE burgers_db;
 
-CREATE TABLE burgers_db
+CREATE TABLE burgers
 (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(140),
